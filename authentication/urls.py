602926d7f07a6da1b55d253c86cf26e232245bbe
Ali <poold189@gmail.com>
2024-08-01 00:53:01 +0500

    Changed TimeZone

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 # Define URL patterns
 urlpatterns = [
     path('', login_page, name="home"),      # Home page
-    path("admin/", admin.site.urls),          # Admin interface
+    # path("admin/", admin.site.urls),          # Admin interface
     path('login/', login_page, name='login_page'),    # Login page
     path('register/', register_page, name='register'),  # Registration page
 ]
